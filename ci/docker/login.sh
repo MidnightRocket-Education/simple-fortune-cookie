245aae0ee2803a36f2c40eaff47362a7de63af09
MidnightRocket <git@midnightrocket.dev>
2024-08-15 14:53:30 +0200

    Add Docker build and push to registry

---
+++ ci/docker/login.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+print() { printf "%b%b" "${1-""}" "${2-"\\n"}"; }
+stderr() { print "$@" 1>&2; }
+reportError() { stderr "$2"; return "$1"; }
+commandv() { command -v "$1" || reportError "$?" "Executable '$1' not found"; }
+
+docker="$(commandv docker)"
+
+if [ -z "${DOCKER_PASSWORD+x}" ]; then
+	reportError 1 "DOCKER_PASSWORD missing"
+fi
+if [ -z "${DOCKER_USER+x}" ]; then
+	reportError 1 "DOCKER_USER missing"
+fi
+
+
+VERSION="$(git describe --tags --match="[vV][[:digit:]]*" --always HEAD)"
+
+echo "$DOCKER_PASSWORD" | docker login "ghcr.io" -u "$DOCKER_USER" --password-stdin
