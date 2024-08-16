57257f43bc51499d01d57ab03a3dfbb868a10d3e
MidnightRocket <git@midnightrocket.dev>
2024-08-16 23:31:54 +0200

    Refactor deployment pipeline to support call from other workflow
    This also means supporting different environments.
    
    The script is refactored to use the `KUBECONFIG` variable instead.
    This enables the script to take an ACTION argument, which can be used
    to specify either 'apply' or 'delete' the deployments.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
-kubectl --kubeconfig $1 apply -f ./kubernetes-configurations/backend
-kubectl --kubeconfig $1 apply -f ./kubernetes-configurations/frontend
-kubectl --kubeconfig $1 apply -f ./kubernetes-configurations/database
+set -eu
+print() { printf "%b%b" "${1-""}" "${2-"\\n"}"; }
+stderr() { print "$@" 1>&2; }
+reportError() { stderr "$2"; return "$1"; }
+
+
+if [ -z "${KUBECONFIG+x}" ]; then
+	reportError 1 "Missing KUBECONFIG env variable"
+fi
+
+
+ACTION="${1:-"apply"}" # Default action is to apply
+
+
+case "$ACTION" in
+	"apply" | "delete" )
+		: # do nothing
+		;;
+	* )
+		reportError 1 "'$ACTION' invalid action. Use either 'apply' or 'delete'"
+		;;
+esac
+
+
+
+
+kubectl "$ACTION" \
+	-f ./kubernetes-configurations/backend \
+	-f ./kubernetes-configurations/frontend \
+	-f ./kubernetes-configurations/database
